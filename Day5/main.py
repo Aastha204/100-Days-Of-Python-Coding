6f208391412d9f6fde4020b76f807f6325899da2
Aastha 204 <aasthagarg333@gmail.com>
2024-01-05 12:55:42 +0530

    Initial commit

---
+++ Day5/main.py
@@ -0,0 +1,17 @@
+# one line copmment 
+'''
+multiple line comment
+we can use this for long comments 
+Increase readability
+'''
+
+print("Print in only one line")
+print("========================================")
+print('''Way
+to 
+write in another line 
+''')
+print("=========================================")
+# same we can do by \n
+
+print('''Way \nto \nwrite in another line ''')
