6ee676a0f253f7ec35168040f512420cd9d23832
Aastha 204 <aasthagarg333@gmail.com>
2024-01-06 16:50:37 +0530

    Initial commit

---
+++ Day6/main.py
@@ -0,0 +1,24 @@
+integerNumber=10
+floatNumber=10.5
+string='a'
+ComplexNumber=4+6j   #also complex(4,6) will give same output
+listType=[5,9,0]
+SetType={"uh","ui"}
+TupleType=(7,9,6)
+Dict={"Aastha":1,"Kriti":2}
+Bool=True
+print(type(Bool))
+NullType=None
+
+
+print(type(integerNumber))
+print(type(floatNumber))
+print(type(string))
+#print(a+c)  #will give error
+print(type(ComplexNumber))
+print(type(listType))
+print(type(SetType))
+print(type(TupleType))
+print(type(Dict))
+print(type(Bool))
+print(type(NullType))
