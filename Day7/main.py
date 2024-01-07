882f0c204f925695ab5dfd02f4e72db9a0c36267
Aastha 204 <aasthagarg333@gmail.com>
2024-01-07 15:06:49 +0530

    Initial commit

--- Day7/main.py
+++ Day7/main.py
@@ -1,6 +1,45 @@
+#Arithmetic Operator
 print(3+4)  #addition
 print(6-3)  #subtraction
+print(2*4)  #multiplication
 print(4%2)  #modulus
 print(4//2) #integer division
 print(4/2)  #float division
 print(2**2) #power
+
+#Comparison Operator
+print(3>8)
+print(3<8)
+print(3==4)
+print(3!=8)
+print(3<=3)
+print(4>=5)
+
+#Logical Operator
+print(1<3 and 4<7)  #it will give True/False
+print(1 or 1)       #it will give 1/0
+print(1 or 0)
+print(not 1==1)
+
+#Assignment operator
+x = 3
+x += 2
+print(x) 
+x -= 2
+print(x)
+y=9
+y/=3
+print(y)
+y*=3
+print(y)
+z=4
+z%=2
+print(z)
+
+#Bitwise operator
+print(1 & 1)
+print(1 | 0)
+print(1^2)
+print(~1)
+print(2<<1)
+print(2>>1)
