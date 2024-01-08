f4bf1a5a627c5cff55ef45bf7f1c75787a7900e2
Aastha 204 <aasthagarg333@gmail.com>
2024-01-08 14:51:05 +0530

    Initial commit

---
+++ Day8/main.py
@@ -0,0 +1,11 @@
+#Calculator
+
+a=39
+b=3
+
+print(f"{a} + {b} is = {a+b}")
+print(f"{a} - {b} is = {a-b}")
+print(f"{a} * {b} is = {a*b}")
+print(f"{a} / {b} is = {a/b}")
+print(f"{a} ** {b} is = {a**b}")
+print(f"{a} // {b} is = {a//b}")
